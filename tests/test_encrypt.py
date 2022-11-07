d53c2f16a949c322a134d4bc506f1534cf731ec5
JennyShen10792 <js10792@nyu.edu>
2022-11-07 15:05:50 -0500

    Add tests for encryption

---
+++ tests/test_encrypt.py
@@ -0,0 +1,18 @@
+import pytest
+
+class Tests:
+	def test_odd_chars(self):
+		encrypted = encrypt.encryptFunction("hello")
+		assert encrypted == "morqu{hnwpx'mw)"
+	
+	def test_even_chars(self):
+		encrypted = encrypt.encryptFunction("test")
+		assert encrypted == "tvyvz#w})iq}"
+		
+	def test_len_2(self):
+		encrypted = encrypt.encryptFunction("~}")
+		assert encrypted == "\"$'\"&,"
+	
+	def test_len_1(self):
+		encrypted = encrypt.encryptFunction("A")
+		assert encrypted == "BDG"
