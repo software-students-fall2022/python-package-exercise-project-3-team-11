5e172d49016fcb9a6350083736597ca81b7f4462
JennyShen10792 <js10792@nyu.edu>
2022-11-06 21:29:07 -0500

    Edit encrypt.py

--- encrypt.py
+++ encrypt.py
@@ -1,6 +1,6 @@
 def encrypt (string):
-	nonencrypted = string
-	first_l = [ord(c) for c in s]
+	nonencrypted = string[::-1]
+	first_l = [ord(c) for c in nonencrypted]
 	swapped_l = []
 	for i in range(0, len(first_l)/2):
 		if len(first_l)/2 % 2 == 0:
@@ -15,11 +15,6 @@ def encrypt (string):
 			swapped_l[j] = 126
 		else:
 			swapped_l[j] = swapped_l[j]-1
-	for k in range(0, len(swapped_l)):
-		if swapped_l[k] > 64:
-			swapped_l[k] = swapped_l[k]*2-126+33
-		else:
-			swapped_l[k] = swapped_l[k]*2
 	encrypted = ""
 	for i in swapped_l:
     	encrypted = encrypted + chr(i)
