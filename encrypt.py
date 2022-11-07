f73776fad97da0371fb0fc839249ea8971222c1f
JennyShen10792 <js10792@nyu.edu>
2022-11-07 10:04:15 -0500

    Edit encrypt.py

--- encrypt.py
+++ encrypt.py
@@ -11,10 +11,27 @@ def encrypt (string):
 			swapped_l.append(first_l[2*i])
 			swapped_l.append(first_l[(len(first_l)/2)-1])
 	for j in range(0, len(swapped_l)):	
-		if swapped_l[j] == 33:
-			swapped_l[j] = 126
+		if swapped_l[j]+(j+1) > 126:
+			first = swapped_l[j]+(j+1)-126+33
+			swapped_l[j]=first
+			second = first + 2*(j+1)
+			if second > 126:
+				second = second-126+33
+			swapped_l.insert(j+1, second)
+			third = second + 3*(j+1)
+			if third > 126:
+				third = third-126+33
+			swapped_l.insert(j+2, third)
 		else:
-			swapped_l[j] = swapped_l[j]-1
+			first = swapped_l[j]+(j+1)
+			second = first + 2*(j+1)
+			if second > 126:
+				second = second-126+33
+			swapped_l.insert(j+1, second)
+			third = second + 3*(j+1)
+			if third > 126:
+				third = third-126+33
+			swapped_l.insert(j+2, third)
 	encrypted = ""
 	for i in swapped_l:
     	encrypted = encrypted + chr(i)
