196e5457cc0f2aa0fdca119becc41aa5b21047d7
les5185 <70672721+les5185@users.noreply.github.com>
2022-11-08 13:00:47 -0500

    Delete test for now

--- tests/test_validate.py
+++
@@ -1,7 +0,0 @@
-import pytest
-from passwordManager import validate
-
-class Tests:
-	def test_input_below_6(self):
-		validate = validate.validate_password("hello")
-		assert validate.validate_password(password) == True
